f01432ad65d40bfd2414aeb55049a514b7800b4c
TranBinh2311 <binhlinh473@gmail.com>
2021-12-09 22:51:52 +0700

    Missing footer

--- public/js/home.js
+++ public/js/home.js
@@ -1,12 +1,18 @@
-const blogSection = document.querySelector('.blogs-section');
-
-db.collection("blogs").get().then((blogs) => {
-    blogs.forEach(blog => {
-        if(blog.id != decodeURI(location.pathname.split("/").pop())){
-            createBlog(blog);
-        }
+const blogSection = document.querySelector('.blogs-section')
+try{
+    db.collection("blogs").get().then((blogs) => {
+        blogs.forEach(blog => {
+            console.log(blog.id);
+            if(blog.id != decodeURI(location.pathname.split("/").pop())){
+                createBlog(blog);
+            }
+        })
     })
-})
+    
+}
+catch(err){
+    console.log(err);
+}
 
 const createBlog = (blog) => {
     let data = blog.data();
